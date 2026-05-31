dfce01a4d98c4dcf0899cd0e40ccba6ca9877468
chaudhary94 <surendra.kumar@ens.enterprises>
2026-05-31 17:38:42 +0530

    add vercel all

--- server.js
+++ server.js
@@ -9,42 +9,48 @@ dotenv.config();
 
 const app = express();
 
-
-// MIDDLEWARE
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-
-// STATIC FOLDER
+// Static Folder
 app.use("/uploads", express.static("uploads"));
 
+// Test Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Expense API Running Successfully",
+  });
+});
 
-// ROUTES
+// API Routes
 app.use("/api/expenses", expenseRoutes);
 
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err);
+  });
 
+// Local Development Only
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 8000;
 
-// DATABASE CONNECTION
-mongoose.connect(process.env.MONGO_URI)
-
-.then(() => {
-  console.log("MongoDB Connected Successfully");
-})
-
-.catch((err) => {
-  console.log(err);
-});
-
-
-
-// SERVER
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
 
 export default app;
