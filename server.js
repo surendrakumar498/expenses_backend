02eee617ac0ce8f81c2c1af3bb58a98dc9337249
chaudhary94 <surendra.kumar@ens.enterprises>
2026-07-09 10:22:50 +0530

    cors update

--- server.js
+++ server.js
@@ -11,18 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-// import cors from "cors";
-
-app.use(
-  cors({
-    origin: [
-      
-      "https://expenses-frontend-nine.vercel.app/",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
